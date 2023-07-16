0cc9c77336c33b1ab87a3e1ae367813bec516710
Dmitry Uvarov <diman_niks@mail.ru>
2023-07-16 04:02:29 +0300

    feat: remove fail test

--- src/example.test.tsx
+++ src/example.test.tsx
@@ -9,10 +9,6 @@ import { Provider } from "react-redux";
 import { initStore } from "./store";
 import { Application } from "./Application";
 
-it('fail test', () => {
-  expect(false).toEqual(true);
-});
-
 it('по адресу /about должна открываться страница "о проекте"', () => {
   const store = initStore();
   const application = (
